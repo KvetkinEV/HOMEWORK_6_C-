693d04cb5c31fd6c77fb3d3e0bf8c85c4b338f42
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-22 18:25:00 +0300

    Programm 3

---
+++ 3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите размерность массива");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите {0}-й элемент", i + 1);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+
+                sum += arr[i] * arr[arr.Length - 1 - i];
+            }
+
+            Console.WriteLine("Сумма произведений пар чисел в одномерном целочисленном массиве: " + sum);
+            Console.ReadLine();
+
