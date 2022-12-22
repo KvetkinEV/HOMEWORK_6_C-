2eb8a7f55af9ef4f344f6195334ad8f597242011
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-22 18:21:49 +0300

    Programm 1

---
+++ 1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите кол-во чисел, которое хотите ввести");
+            List<int> numbers = new List<int>();
+            int M = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите числa:");
+            for (int i = 0; i < M; i++)
+            {
+                numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("Чисел больше нуля: " + numbers.Where(n => n > 0).Count());
+
+            Console.ReadLine();
+
